d7233461fe1679044eecd285e81c7c32d8ec9ec1
Yadav <ayadav@dovercorp.com>
2021-09-14 20:29:29 +0530

    initial commit of project phase 2

---
+++ projectAssesment2/src/app/customers/customer.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Customer } from './customer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private static customers:Customer[] = [
+    {id:1001, name:'Akshat', gender:'Male', city:'Kolkate', phone:'9559325102',uname:'akshat',password:'password'},
+    {id:1001, name:'Anil', gender:'Male', city:'Hyderabad', phone:'987654321',uname:'anil',password:'password'},
+    {id:1002, name:'Bobby', gender:'Male', city:'Bengaluru', phone:'987654320',uname:'bobby',password:'password'},
+    {id:1003, name:'Kavya', gender:'Female', city:'Chennai', phone:'987654329',uname:'kavya',password:'password'}
+  ];
+  constructor() { }
+  static getCustomers():Customer[]{
+    return this.customers;
+  }
+  static addCustomer(c:Customer):void{
+    this.customers.push(c);
+  }
+}
